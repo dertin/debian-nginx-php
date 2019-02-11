97ea317e4ab6ff6358df3ae5cc585e504df0eb0f
dertin <dev.dertin@gmail.com>
2019-02-10 23:39:16 -0300

    Wait for apt

--- .travis/wait_for_apt.sh
+++ .travis/wait_for_apt.sh
@@ -8,6 +8,8 @@ fi
 $SUDO systemctl stop apt-daily.service apt-daily.timer apt-daily-upgrade.service apt-daily-upgrade.timer
 $SUDO systemctl kill --kill-who=all apt-daily.service apt-daily-upgrade.service
 
+$SUDO lsof /var/lib/dpkg/lock
+
 while sleep 1; do
     if [ $($SUDO pgrep apt | wc -l) -lt 1 ] ; then
         echo "apt process done"
